d6516249a6db0296f518e2a4c433ddec6d12ff63
Riski Rahmat Hia <vprozzhia@gmail.com>
2022-11-24 22:42:27 +0700

    comit index.js coba coba tombol

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 var textBesar = document.querySelector(".text-besar");
+var clickPertama = true;
 
 for(x=0; x<document.querySelectorAll(".col").length; x++){
     document.querySelectorAll(".col")[x].addEventListener("click",(e)=>{
         var input = e.target.getAttribute("data-number");
-        textBesar.innerHTML = start(input);
+        var inputSalah = ["0","00","000"];
+        if(clickPertama && inputSalah.includes(input)){
+            textBesar.innerHTML = "";
+            if(inputSalah.includes(start(input))){
+                textBesar.innerHTML = "0";
+            }else{
+                textBesar.innerHTML += start(input);
+            }
+            clickPertama = false;
+        }else{
+            textBesar.innerHTML += start(input);
+        }
     });
 }
 
 function start(tombolParam){
     var tombolBoleh = ["clear","del","/100","/","x","+","-","equal"];
     if(tombolBoleh.includes(tombolParam)){
-        console.log("tidak berfungsi");
+        return "";
     }else{
-        textBesar.innerHTML = "";
         return tombolParam;
     }
 }
